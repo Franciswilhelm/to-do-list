aa6501f27cbddd967b21881b01bb270cc59df3e2
Caleb Paul <calebpaul21@gmail.com>
2016-08-19 10:12:54 -0700

    achieve 1st project spec by counting up to a user given input in a ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 //Business Logic
+var range = 0;
+var textList = [];
 
+function numList(number) {
+  for (var i = 1; i <= number; i++) {
+    range = i;
+    textList.push(i);
+    console.log(textList);
+  }
+  return(textList);
+}
 
 //User Logic
-$( document ).ready(function() {
-  $form("#NAME_form").submit(function(event){
-  event.preventDefault();
+$(document).ready(function() {
+  $("#pong_form").submit(function(event){
+    event.preventDefault();
+    var startNum = parseInt($("#num_input").val());
+    var output = numList(startNum)
 
-
-  })
+    function frontEnd(){
+      var nums = "";
+      for (var i = 0; i < textList.length; i++) {
+        nums += "<li>" + textList[i] + "</li>";
+        console.log(nums)
+      }
+      $("#pong_output").append(nums);
+    }
+    frontEnd(textList);
+  });
 });
--- notes.md
+++ notes.md
@@ -8,3 +8,7 @@ Numbers divisible by 15 are replaced with "ping-pong"
 A user should be able to enter a new number and see new results over and over again.
 
 Break the program down into simple, individual behaviors with input/output examples. Begin with the simplest possible behavior. List these behaviors (also known as "specs" or "specifications") in the project's README.md file.
+
+Business logic: The code responsible for handling the evaluation and manipulation of data; does not require any user interface.
+
+User interface logic: The code responsible for the interaction between the user and the application; handles tasks such as event listening, user input forms, DOM manipulation, display and styling. We have not covered how to write the actual code for this yet; don't worry!
