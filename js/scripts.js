d68f309ea55cd7b8b2fee39817ed28665a3a20a3
Caleb Paul <calebpaul21@gmail.com>
2016-08-19 12:54:57 -0700

    fix another bug in a function that was breaking 1st spec

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,20 @@ var pong5 = "pong";
 var pingPong15 = "ping-pong"
 
 function numList(number) {
+  textList.splice(0);
   textList.push(0);
   for (var i = 1; i <= number; i++) {
     textList.push(i);
-    /*if (i % 15 === 0) {
+    if (i % 15 === 0) {
       textList.splice(i, 1, pingPong15)
     } else if (i % 5 === 0) {
       textList.splice(i, 1, pong5)
-    } else*/ if (i % 3 === 0) {
-      // alert(i);
+    } else if (i % 3 === 0) {
       textList.splice(i, 1, ping3)
     }
     console.log(textList);
   }
-  return(textList);
+  return(textList.splice(0, 1));
 }
 //User Logic
 $(document).ready(function() {
@@ -33,7 +33,7 @@ $(document).ready(function() {
         nums += "<li>" + textList[i] + "</li>";
         console.log(nums)
       }
-      $("#pong_output").append(nums);
+      $("#pong_output").empty().append(nums);
     }
     arrayToList(textList);
   });
