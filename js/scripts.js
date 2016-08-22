0678d8cc7b1ec2da79d3d8a21604bf0da74b108c
Caleb Paul and Francis Prus <francis@>
2016-08-22 14:04:00 -0700

    add html forms and js objects/variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 //Business Logic
+function ToDo(priorityItems, items) {
+  this.priorityItems = priorityItems;
+  this.items = items;
+  // this.completed = false;
+}
 
 //User Logic
 $(document).ready(function() {
-
+  $("form#new-form").submit(function() {
     event.preventDefault();
 
+    var userPriorityItem = ("input#new-priority-item").val();
+    var userItem = ("input#new-item").val();
+
+    var newToDo = new ToDo(userPriorityItem, userItem);
+
+    console.log(userItem);
+    console.log(userPriorityItem);
+    console.log(newToDo);
+
+    $("#ul-list").prepend("<li><span>" + newToDo.userPriorityItem + "</span></li>");
+    $("#ul-list").append("<li><span>" + newToDo.userItem + "</span></li>");
+  });
 });
